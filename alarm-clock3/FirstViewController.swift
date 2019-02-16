c5724129d3332a8b46c0d2a0eceb86d7dd96d29a
Christine Yoon <cy125@duke.edu>
2019-02-16 16:13:13 -0500

    Initial Commit

---
+++ alarm-clock3/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  alarm-clock3
+//
+//  Created by Christine Yoon on 2/16/19.
+//  Copyright © 2019 Christine Yoon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ alarm-clock3/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  alarm-clock3
+//
+//  Created by Christine Yoon on 2/16/19.
+//  Copyright © 2019 Christine Yoon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
